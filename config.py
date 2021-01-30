8bbe59073629e347534ee8dc6ee92879cb2c15d7
Diogo Paulico <55745304+Diogo-Paulico@users.noreply.github.com>
2021-01-30 01:46:32 +0000

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+import datetime
+
+# Login information ( works with GMAIL after enabling insecure apps)
+SENDER_EMAIL = ''
+PASSWORD = ''
+
+# SMTP Server Settings
+SMTP_ADRESS = "smtp.gmail.com"
+SSL_PORT = 465
+
+#EMAIL RECEIVERS CSV FILE NAME (MUST CONTAIN A COLLOM NAMED "EMAIL")
+CSV_FILE = "contacts.csv"
+
+#TEXT FILE CONTAINING THE EMAIL BODY
+TEXT_BODY_FILE = ''
+
+# EMAIL SUBJECT
+SUBJECT = ''
+
---
+++ pd_email.py
@@ -0,0 +1,45 @@
+import email, smtplib, ssl, datetime, poplib
+import pandas as pd
+
+from config import *
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+def buildContacts():
+    df = pd.read_csv(CSV_FILE)
+    contacts = df.Email
+    return contacts
+
+emailBodyFile = open(TEXT_BODY_FILE,'r')
+
+body = emailBodyFile.read()
+
+
+def messageBuilder(email_receiver):
+        message = MIMEMultipart()
+        message["From"] = SENDER_EMAIL
+        message["To"] = email_receiver
+        message["Subject"] = SUBJECT
+
+        message.attach(MIMEText(body, "plain"))
+        return message.as_string()
+
+def emailSend():
+    contacts = buildContacts()
+    counter = 0
+    
+    context = ssl.create_default_context() 
+    with smtplib.SMTP_SSL(SMTP_ADRESS, SSL_PORT, context=context) as server:
+            try: 
+                server.login(SENDER_EMAIL, PASSWORD)
+                for receiver_email in contacts:      
+                    server.sendmail(SENDER_EMAIL, receiver_email, messageBuilder(receiver_email))
+                    counter+=1
+            except:
+                print("SMTP server connection error")
+        
+    print('Sent ' + str(counter) + ' out of '+ str(contacts.size) + ' emails.')
+
+emailSend()
