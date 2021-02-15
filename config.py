cea9eb24bd54dbeaed3d056853421a2b109ef9e5
Diogo Paulico <d.paulico@campus.fct.unl.pt>
2021-02-15 23:35:11 +0000

    Updated config.py

--- config.py
+++ config.py
@@ -1,19 +1,33 @@
-import datetime
-
-# Login information ( works with GMAIL after enabling insecure apps)
-SENDER_EMAIL = ''
+# Login information (works with GMAIL after enabling insecure apps)
+SENDER_EMAIL = ""
 PASSWORD = ''
+
+
+SENDER_NAME = ''
 
 # SMTP Server Settings
 SMTP_ADRESS = "smtp.gmail.com"
 SSL_PORT = 465
 
 #EMAIL RECEIVERS CSV FILE NAME (MUST CONTAIN A COLLOM NAMED "EMAIL")
-CSV_FILE = "contacts.csv"
+CSV_FILE = ""
 
 #TEXT FILE CONTAINING THE EMAIL BODY
-TEXT_BODY_FILE = ''
+TEXT_BODY_FILE = ""
+
 
 # EMAIL SUBJECT
 SUBJECT = ''
 
+#Unsubscibing Section
+EMAIL_FOLDER = "Inbox"
+
+IMAP_ADRESS = 'imap.gmail.com'
+
+CANCEL_SUBJECT_KEYWORD = ''
+
+SEND_UNSUB_MESSAGE = True
+
+UNSUB_MESSAGE_SUBJECT = ''
+
+UNSUB_BODY_FILE = ''
